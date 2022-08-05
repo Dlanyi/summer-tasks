9105789e7f4497af3d6bef01dd85f281181ee582
Lotte15 <109030057+Lotte15@users.noreply.github.com>
2022-08-05 14:26:29 +0800

    Add files via upload

---
+++ week-04/06/js/index.js
@@ -0,0 +1,71 @@
+$(function () {
+  // 使用 ajax 获取 userList.json 数据并渲染到页面
+  getData();
+
+  // 为按钮添加事件
+  $("#add").click(function () {
+    // TODO：补充代码，实现功能
+    let option = $("#leftSelect option:selected");
+    //遍历jQ获取的节点
+    option.each((index, item) => {
+      $(`#leftSelect option[value=${item.value}]`).remove();
+      $("#rightSelect")[0].add(new Option(item.value, item.value));
+    });
+    changeAccess("管理员", option);
+  });
+  $("#addAll").click(function () {
+    // TODO：补充代码，实现功能
+    let option = $("#leftSelect option");
+    option.each((index, item) => {
+      $(`#leftSelect option[value=${item.value}]`).remove();
+      $("#rightSelect")[0].add(new Option(item.value, item.value));
+    });
+    changeAccess("管理员", option);
+  });
+  $("#remove").click(function () {
+    // TODO：补充代码，实现功能
+    let option = $("#rightSelect option:selected");
+    option.each((index, item) => {
+      $(`#rightSelect option[value=${item.value}]`).remove();
+      $("#leftSelect")[0].add(new Option(item.value, item.value));
+    });
+    changeAccess("普通用户", option);
+  });
+  $("#removeAll").click(function () {
+    // TODO：补充代码，实现功能
+    let option = $("#rightSelect option");
+    option.each((index, item) => {
+      $(`#rightSelect option[value=${item.value}]`).remove();
+      $("#leftSelect")[0].add(new Option(item.value, item.value));
+    });
+    changeAccess("普通用户", option);
+  });
+});
+
+/**
+ * 修改权限
+ * @param {Object} right 要修改的权限
+ * @param {Object} changeList 要修改权限的用户列表
+ */
+function changeAccess(right, changeList) {
+  // TODO：补充代码，实现功能
+  changeList.each((index, item) => {
+    $(`#userList tr[name=${item.value}] td:last`).html(right);
+  });
+}
+// 异步获取数据
+function getData() {
+  // TODO：补充代码，实现功能
+  $.ajax("./js/userList.json").then((res) => {
+    res.forEach((item) => {
+      // jQ方法：html() 设置html内容
+      $("#userList tbody").html(
+        $("#userList tbody").html() +
+        ` <tr name=${item.name}>
+            <td>${item.name}</td>
+            <td>${item.right ? "管理员" : "普通用户"}</td> 
+          </tr>`
+      );
+    });
+  });
+}
